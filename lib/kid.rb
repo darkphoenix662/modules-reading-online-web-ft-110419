50cf6dd9d5e4d63261aec5a5ec8869e4953708c5
Sean Good <tree662@protonmail.com>
2019-12-04 19:08:39 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -3,5 +3,11 @@ require_relative './class_methods_module.rb'
 require_relative './fancy_dance.rb'
 class Kid 
   extend MetaDancing
-  
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
 end
