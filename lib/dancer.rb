6bf48bd7cd47253c72b29bc378608ee5ecb9a1d6
Sean Good <tree662@protonmail.com>
2019-12-04 19:08:53 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -3,5 +3,11 @@ require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 class Dancer
   extend MetaDancing 
-  
+  include Dance
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
 end
